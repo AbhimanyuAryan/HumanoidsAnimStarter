db100c4e9ed2de36cb0a3ef1c5eb56877d927eba
DESKTOP-2LV9GDO\GK_Desk_07 <abhimanyu.chat@gmail.com>
2017-12-14 11:55:11 +0530

    Syncing Animations on Network for Humanoid Character

---
+++ Assets/SetupLocalPlayer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+	Animator animator;
+	[SyncVar (hook="OnChangeAnimation")] public string animState = "idle";
+
+	[Command] public void CmdChangeAnimState(string aS)
+	{
+		UpdateAnimationState(aS);
+	}
+
+	void OnChangeAnimation(string aS)
+	{
+		if(isLocalPlayer) return;
+		UpdateAnimationState(aS);
+	}
+
+	void UpdateAnimationState(string aS)
+	{
+		if(animState == aS) return;
+		animState = aS;
+		Debug.Log(animState);
+		if(animState == "idle") 
+			animator.SetBool("Idling", true);
+		else if(animState == "run")
+			animator.SetBool("Idling", false);
+		else if(animState == "attack")
+			animator.SetTrigger("Attacking");
+	}
+
+	void Start () {
+		animator = GetComponent<Animator>(); 
+		animator.SetBool("Idling", true);
+
+		if(isLocalPlayer)
+		{
+			GetComponent<PlayerController>().enabled = true;
+			CameraFollow360.player = this.gameObject.transform;
+		}
+		else
+		{
+			GetComponent<PlayerController>().enabled = false;			
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
